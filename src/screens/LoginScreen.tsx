4f89bd1c5b761b9315f7fb070ac0eef932144a9d
Juan Esteban <juanes0202@gmail.com>
2022-06-17 18:02:16 -0500

    wip

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { View, Text, TextInput, Platform, TouchableOpacity } from 'react-native';
 import Background from '../components/Background';
 import WhiteLogo from '../components/WhiteLogo';
+import { loginStyles } from '../theme/loginTheme';
 
 const LoginScreen = () => {
     return (
@@ -11,6 +12,51 @@ const LoginScreen = () => {
 
             {/* Keyboard avoid view */}
             <WhiteLogo />
+
+            <Text style={loginStyles.title}>Login</Text>
+
+            <Text style={loginStyles.label}>Email:</Text>
+
+            <TextInput
+                placeholder="Enter your email"
+                placeholderTextColor="rgba(255, 255, 255, 0.4)"
+                keyboardType="email-address"
+                underlineColorAndroid="white"
+                style={[
+                    loginStyles.inputField,
+                    (Platform.OS === 'ios') && loginStyles.inputFieldIos,
+                ]}
+                selectionColor="white"
+
+                // TODO: onchange, value
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+
+            <TextInput
+                placeholder="*******"
+                placeholderTextColor="rgba(255, 255, 255, 0.4)"
+                underlineColorAndroid="white"
+                style={[
+                    loginStyles.inputField,
+                    (Platform.OS === 'ios') && loginStyles.inputFieldIos,
+                ]}
+                selectionColor="white"
+
+                // TODO: onchange, value
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+
+            {/* Login Button */}
+            <View style={loginStyles.buttonContainer}>
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={loginStyles.button}
+                >
+                    <Text style={loginStyles.buttonText}>Login</Text>
+                </TouchableOpacity>
+            </View>
         </>
     );
 };
--- src/theme/loginTheme.tsx
+++ src/theme/loginTheme.tsx
@@ -1,5 +1,45 @@
 import { StyleSheet } from 'react-native';
 
-const loginStyles = StyleSheet.create({
+export const loginStyles = StyleSheet.create({
+    title: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginTop: 20,
+    },
 
+    label: {
+        marginTop: 25,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+
+    inputField: {
+        color: 'white',
+        fontSize: 20,
+    },
+
+    inputFieldIos: {
+        borderBottomColor: 'white',
+        borderBottomWidth: 2,
+        marginBottom: 4,
+    },
+
+    buttonContainer: {
+        alignItems: 'center',
+        marginTop: 50,
+    },
+
+    button: {
+        borderWidth: 2,
+        borderColor: 'white',
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        borderRadius: 100,
+    },
+
+    buttonText:{
+        fontSize: 18,
+        color: 'white',
+    },
 });
