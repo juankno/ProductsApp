700b859c3e8e5b89e39ded16ff25d128316631a5
Juan Esteban <juanes0202@gmail.com>
2022-06-18 12:49:16 -0500

    creando auth reducer

---
+++ src/context/AuthReducer.tsx
@@ -0,0 +1,37 @@
+import { Usuario } from '../interfaces/loginResponse';
+
+export interface AuthState {
+    status: 'checking' | 'authenticated' | 'not-authenticated';
+    token: string | null;
+    errorMessage: string;
+    user: Usuario | null;
+}
+
+export type AuthAction =
+    | { type: 'signUp', payload: { token: string, user: Usuario } }
+    | { type: 'addError', payload: string }
+    | { type: 'removeError' }
+    | { type: 'notAuthenticated' }
+    | { type: 'logout' }
+
+
+export const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+    switch (action.type) {
+        case 'addError':
+            return {
+                ...state,
+                user: null,
+                status: 'not-authenticated',
+                token: null,
+                errorMessage: action.payload,
+            };
+        case 'removeError':
+            return {
+                ...state,
+                errorMessage: '',
+            };
+
+        default:
+            return state;
+    }
+};
